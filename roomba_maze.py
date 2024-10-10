ad849b548959813a7e7ca0a7d2f01afa42b2e788
Ivan Sergeev <ivan.sergeev@ist.ac.at>
2024-10-10 19:09:56 +0200

    Implemented checking of reachability of top left corner in two mazes simultaneously.

--- roomba_maze.py
+++ roomba_maze.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List, Tuple
 from typing_extensions import Self
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -6,6 +6,30 @@ import random
 
 
 class Maze:
+    # Stores a maze
+
+    @staticmethod
+    def cardinal_directions() -> List[Tuple[int, int]]:
+        return [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    @staticmethod
+    def are_neighbors(self, x1: int, y1: int, x2: int, y2: int) -> bool:
+        return abs(x1 - x2) + abs(y1 - y2) == 1
+
+    def is_in_bounds(self, x: int, y: int) -> bool:
+        return 0 <= x < self.size_x and 0 <= y < self.size_y
+
+    def move(self, x: int, y: int, direction: int) -> Tuple[int, int]:
+        dx, dy = self.cardinal_directions()[direction]
+        res = (x + dx, y + dy)
+        return res if self.is_in_bounds(*res) and self.G.has_edge((x, y), res) else (x, y)
+
+    def neighbors_of(self, x: int, y: int) -> List[int]:
+        return [
+            (x + dx, y + dy) for (dx, dy) in self.cardinal_directions()
+            if self.is_in_bounds(x + dx, y + dy)
+        ]
+
     def new_empty(self, size_x: int, size_y: int):
         # Creates an empty rectangular maze with dimensions size_x x size_y
         self.size_x, self.size_y = size_x, size_y
@@ -13,18 +37,11 @@ class Maze:
         self.G.add_nodes_from([(x, y) for x in range(size_x) for y in range(size_y)])
         return self
 
-    def random_neighbor(self, x: int, y: int) -> Tuple[int, int]:
-        # Returns a random neighbor in bounds
-        # todo: control RNG seed
-        neighbors = [
-            (x + dx, y + dy) for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            if 0 <= x + dx < self.size_x and 0 <= y + dy < self.size_y
-        ]
-        return random.choice(neighbors)
-
     def add_random_wilson(self) -> Self:
-        # Creates a random rectangular maze with dimensions size_x x size_y using Wilson's algorithm
+        # Generates a random rectangular maze with dimensions size_x x size_y using Wilson's algorithm
         # The algorithm is described in https://dl.acm.org/doi/10.1145/237814.237880
+
+        # Assumption: `self.G` has nodes representing cells of a rectangle
 
         # Init
         in_maze = {node: False for node in self.G.nodes}
@@ -40,7 +57,7 @@ class Maze:
         for node in self.G.nodes:
             curr = node
             while not in_maze[curr]:
-                next_node[curr] = self.random_neighbor(*curr)
+                next_node[curr] = random.choice(self.neighbors_of(*curr))
                 curr = next_node[curr]
 
             curr = node
@@ -59,15 +76,47 @@ class Maze:
         plt.show()
         return self
 
-    def cartesian_product(self, other: Self) -> nx.Graph:
-        # todo: desc
-        assert(self.size_x == other.size_x and self.size_y == other.size_y)
 
-        product = nx.Graph()
+class ConcurrentMovesGraph:
+    # A directed graph that encodes all possible concurrent moves in two mazes
+    def __init__(self, maze1: Maze, maze2: Maze):
+        self.digraph = nx.DiGraph()
+        self.digraph.add_nodes_from([(u, v) for u in maze1.G.nodes for v in maze2.G.nodes])
+        self.digraph.add_edges_from([
+            ((u, v), (maze1.move(*u, d), maze2.move(*v, d)))  # arc head can be reached from arc tail in 1 move
+            for u in maze1.G.nodes for v in maze2.G.nodes for d in range(len(maze1.cardinal_directions()))
+        ])
 
-        # todo: implement
+    def draw(self) -> Self:
+        # Draws the graph
+        # todo: PyVis for interactive diagram
 
-        return product
+        nx.draw(self.digraph)
+        plt.show()
+        return self
+
+    def num_nodes(self) -> int:
+        # Returns the number of nodes in the graph
+        return self.digraph.number_of_nodes()
+
+    def is_reachable_from_all(self, x1: int, y1: int, x2: int, y2: int) -> bool:
+        # Checks if a given (simultaneous) state in two mazes can be reached from all starting states
+        shortest_paths = nx.single_target_shortest_path(self.digraph, ((x1, y1), (x2, y2)))
+        return len(shortest_paths) == self.num_nodes()
 
 
-maze = Maze().new_empty(5, 5).add_random_wilson().draw()
+def main():
+    size_x, size_y = 10, 10
+    for i in range(100):
+        # print(i)
+        maze1 = Maze().new_empty(size_x, size_y).add_random_wilson()
+        maze2 = Maze().new_empty(size_x, size_y).add_random_wilson()
+        aux_graph = ConcurrentMovesGraph(maze1, maze2)
+        if not aux_graph.is_reachable_from_all(0, 0, 0, 0):
+            print(f'Found counterexample: {maze1.graph} {maze2.graph}')
+            maze1.draw()
+            maze2.draw()
+
+
+if __name__ == '__main__':
+    main()
